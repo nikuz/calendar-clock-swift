29791eafaa47e34309fc2603e3b5b30d4a11c378
Nikolai Kuznetcov <Nikolai.Kuznetcov@motorolasolutions.com>
2026-07-14 18:51:15 -0700

    set swift-tools-version to 6.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version: 6.3
 import PackageDescription
 
 #if os(macOS)
@@ -55,5 +55,6 @@ let package = Package(
                 .linkedLibrary("rt",            .when(platforms: [.linux])),
             ]
         ),
-    ]
+    ],
+    swiftLanguageModes: [.v6]
 )
