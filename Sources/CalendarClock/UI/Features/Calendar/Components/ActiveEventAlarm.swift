6feaf39ac0abb67e4ca1d1d9c8b7653f518612b4
Nikolai Kuznetcov <Nikolai.Kuznetcov@motorolasolutions.com>
2026-07-24 19:01:51 -0700

    ui: limit active event alarm duration to 1 minute

--- Sources/CalendarClock/UI/Features/Calendar/Components/ActiveEventAlarm.swift
+++ Sources/CalendarClock/UI/Features/Calendar/Components/ActiveEventAlarm.swift
@@ -2,7 +2,12 @@ import Foundation
 import CRayLib
 
 @MainActor
-struct ActiveEventAlarm {
+enum ActiveEventAlarm {
+    private static let maxAlarmDuration = 60.0 // seconds
+    private static var alarmStarted = false
+    private static var alarmStartTime = 0.0
+    private static var lastActiveEventIndex: Int?
+    
     static func play(
         appState: AppStateData,
         eventsOrder: CalendarUIUtils.EventsOrder,
@@ -21,14 +26,53 @@ struct ActiveEventAlarm {
             StopSound(eventApproachingSound)
         }
 
+        // prevent playing both the approaching and the alarm sounds simultaneously
+        if IsSoundPlaying(eventApproachingSound) {
+            if alarmStarted {
+                resetAlarm()
+            }
+            return
+        }
+
+        let alarmDuration = GetTime() - alarmStartTime
         if let activeEvent = eventsOrder.activeEvent {
-            if activeEvent.event.id != confirmedEvent && !IsSoundPlaying(eventAlarmSound) {
+            if alarmStarted && lastActiveEventIndex != activeEvent.index {
+                resetAlarm()
+            }
+            if (
+                activeEvent.event.id != confirmedEvent 
+                && alarmDuration < maxAlarmDuration 
+                && !IsSoundPlaying(eventAlarmSound)
+            ) {
+                if !alarmStarted {
+                    startAlarm(activeEvent.index)
+                }
                 PlaySound(eventAlarmSound)
-            } else if activeEvent.event.id == confirmedEvent && IsSoundPlaying(eventAlarmSound) {
+            } else if (
+                activeEvent.event.id == confirmedEvent 
+                && IsSoundPlaying(eventAlarmSound)
+            ) {
                 StopSound(eventAlarmSound)
             }
-        } else if IsSoundPlaying(eventAlarmSound) {
-            StopSound(eventAlarmSound)
+        } else {
+            if IsSoundPlaying(eventAlarmSound) {
+                StopSound(eventAlarmSound)
+            }
+            if alarmStarted {
+                resetAlarm()
+            }
         }
+    }
+
+    private static func startAlarm(_ activeEventIndex: Int) {
+        alarmStarted = true
+        alarmStartTime = GetTime()
+        lastActiveEventIndex = activeEventIndex
+    }
+
+    private static func resetAlarm() {
+        alarmStarted = false
+        alarmStartTime = GetTime()
+        lastActiveEventIndex = nil
     }
 }
